14561e6b97b986f57a094962f61db7e4f684fa6f
Srijoni2301 <srijonichoudhary@gmail.com>
2022-06-01 20:00:05 +0530

    final

--- src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -4,11 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author sa
- * @date 6.02.2021
- * @time 14:05
- */
+
 @RestController
 public class ApiController
 {
